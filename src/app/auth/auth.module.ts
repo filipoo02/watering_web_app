b2ab9f1a662844308cd6574721921b97a0abe777
Filip <filipsadowski.os@o2.pl>
2023-04-20 23:39:33 +0200

    fix after resolving conflicts

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -15,11 +15,7 @@ import { reducers } from './store/reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { LoginEffect } from './store/effects/login.effect';
 import { CommonModule } from '@angular/common';
-import {
-  FormsModule,
-  ReactiveFormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/auth/', '.json');
@@ -59,7 +55,6 @@ const routes: Route[] = [
       extend: true,
       defaultLanguage: localStorage.getItem('lang') || 'pl',
     }),
-    ReactiveFormsModule,
     ReactiveFormsModule,
   ],
   declarations: [
